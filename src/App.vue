6ea67717c223450181d32bbe413964659b330862
mcpattigans1992 <pmoore2014@gmail.com>
2021-06-04 16:23:53 -0500

    Adding vuetify plugin to project and making a basic navigation drawer component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      clipped-left
+      color="#092d3d"
+      dark
+    >
+      <v-app-bar-nav-icon @click="isNavDrawerOpen = !isNavDrawerOpen" />
+      <v-toolbar-title>Patrick T. Moore</v-toolbar-title>
+    </v-app-bar>
+    <NavDrawer/>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavDrawer from './components/NavDrawer';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    NavDrawer,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/NavDrawer.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-navigation-drawer dark color="#092d3d" app clipped v-bind="$attrs" class="py-2">
+    <v-list dense nav class="py-0">
+      <v-list-item :to="{ name: 'Phone Lookup' }">
+        <v-list-item-icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>Education</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+  export default {
+    name: 'NavDrawer',
+    data: () => ({}),
+  }
+</script>
